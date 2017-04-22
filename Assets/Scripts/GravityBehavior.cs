040d78cae99ced223a93ae82012717fedb7a7c1e
Dylan Sundberg <dylanmsundberg@gmail.com>
2017-04-21 20:21:36 -0700

    Added Gravity Behavior and Manager

---
+++ Assets/Scripts/GravityBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityBehavior : MonoBehaviour {
+	
+	public Rigidbody2D body;
+	public float mass;
+
+
+	// Use this for initialization
+	void Start () {
+		body = this.GetComponent<Rigidbody2D>();
+		mass = body.mass;
+		GravityManager.RegisterGravityObject(this);
+
+		// Uncomment below to add random starting force
+		//body.AddForce(new Vector2(Random.Range(-200f,200f), Random.Range(-200f,200f)));
+	}
+
+	public Vector2 GetPosition()
+	{
+		return body.position;
+	}
+}
---
+++ Assets/Scripts/GravityManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityManager : MonoBehaviour {
+
+	private static GravityManager instance = null;
+	private static List<GravityBehavior> gravityObjects;
+	public static float G = 6.67408E-11f;
+
+	public static GravityManager SharedInstance {
+		get {
+			if (instance == null) {
+				instance = new GravityManager();
+			}
+			return instance;
+		}
+	}
+
+	void Awake() {
+		gravityObjects = new List<GravityBehavior>();
+	}
+
+	public static void RegisterGravityObject(GravityBehavior obj) {
+		if (!gravityObjects.Contains(obj))
+		{
+			gravityObjects.Add(obj);
+			Debug.Log("Gravity Object added!");
+		}
+	}
+
+	void Update() {
+		foreach(GravityBehavior giveObj in gravityObjects)
+		{
+			foreach(GravityBehavior receiveObj in gravityObjects)
+			{
+				if (giveObj != receiveObj)
+					ApplyPullForce(giveObj,receiveObj);
+			}
+		}
+	}
+		
+	void ApplyPullForce(GravityBehavior obj1, GravityBehavior obj2)
+	{
+		Vector2 pos1 = obj1.GetPosition();
+		Vector2 pos2 = obj2.GetPosition();
+
+		float radius = Vector2.Distance(pos1, pos2);
+
+		Vector2 force = (pos1 - pos2).normalized * 10E10f * (G * obj1.mass * obj2.mass) / (radius * radius);
+
+		obj2.body.AddForce(force);
+	}
+}
