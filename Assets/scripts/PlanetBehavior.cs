dea50e9144666a608528aecb74c101f856697c9f
Dylan Sundberg <dylanmsundberg@gmail.com>
2017-04-23 12:41:30 -0700

    Audio Setting and planet drone

--- Assets/scripts/PlanetBehavior.cs
+++ Assets/scripts/PlanetBehavior.cs
@@ -10,9 +10,18 @@ public class PlanetBehavior : GravityBehavior {
 	public int population;
 	public GameObject[] planetTemplates;
 	public float size;
+	AudioHighPassFilter hiPass;
+	AudioListener listener;
+	Rigidbody2D listenerBody;
+	AudioSource planetAudio;
+	float cutoff;
+	float volume;
 
 	public override void Start(){
-
+		listener = GameObject.FindObjectOfType<AudioListener>();
+		listenerBody = listener.GetComponent<Rigidbody2D>();
+		hiPass = GetComponent<AudioHighPassFilter>();
+		planetAudio = GetComponent<AudioSource>();
 		GameObject planet =  Instantiate(planetTemplates[(int)Mathf.Floor(Random.Range(0, planetTemplates.Length))], transform.position, Quaternion.identity) as GameObject;
 		planet.transform.parent = transform;
 
@@ -20,15 +29,25 @@ public class PlanetBehavior : GravityBehavior {
 		size = Random.Range(minSize, maxSize);
 		transform.localScale = Vector3.one * size;
 		UpdateMass(mass * size + 1);
+		planetAudio.pitch = 0.8f/size;
 	}
 
 	void OnAwake() {
 		population = 0;
+		cutoff = 0f;
+		volume = 0f;
 	}
 
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+		float soundDistance = Vector2.Distance(listener.transform.position, transform.position);
+		if (soundDistance < planetAudio.maxDistance)
+		{
+			cutoff = Mathf.SmoothStep(cutoff, Mathf.Clamp((soundDistance-1f)*50f, 20f, 400f), 10*Time.deltaTime);
+			hiPass.cutoffFrequency = cutoff;
+			volume = Mathf.SmoothStep(volume, (body.velocity.sqrMagnitude/100f)*0.3f + 0.7f*(listenerBody.velocity - body.velocity).sqrMagnitude/100f, 10*Time.deltaTime);
+			planetAudio.volume = Mathf.Clamp(volume, 0f, 0.7f);
+		}
 	}
 
 	void OnCollisionEnter2D(Collision2D collision){
