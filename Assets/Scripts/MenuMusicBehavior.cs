591a582c6071c0e5930877ef14bec7f3fd35b29c
Dylan Sundberg <dylanmsundberg@gmail.com>
2017-04-23 22:06:30 -0700

    Adding music and player sounds

---
+++ Assets/Scripts/MenuMusicBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMusicBehavior : MonoBehaviour {
+
+	AudioSource music;
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad(gameObject);
+		music = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!music.isPlaying)
+			Destroy(gameObject);
+	}
+}
