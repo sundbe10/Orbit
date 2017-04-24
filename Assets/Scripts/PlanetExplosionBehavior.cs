ecae6d53cf375a3f92314729cc405648155cf4c9
Dylan Sundberg <dylanmsundberg@gmail.com>
2017-04-23 17:47:49 -0700

    Added Explosions, fixed loops

---
+++ Assets/Scripts/PlanetExplosionBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetExplosionBehavior : MonoBehaviour {
+
+	AudioSource sound;
+	public List<AudioClip> sounds;
+
+	// Use this for initialization
+	void Start () {
+		sound = GetComponent<AudioSource>();
+		sound.clip = sounds[Random.Range(0, sounds.Count)];
+		sound.pitch = Random.Range(.85f,.95f);
+		sound.volume = Random.Range(.9f,1f);
+		sound.Play();
+	}
+}
